8ea338af907507d5f7987e24413938128620fd03
Anuchit Chalothorn <anoochit@gmail.com>
2017-05-13 08:55:43 +0700

    add near api

--- routes/place.js
+++ routes/place.js
@@ -13,16 +13,12 @@ exports.register = function(server, options, next) {
       method: 'GET',
       path: '/place',
       handler: function (request, reply) {
-
           db.place.find((err, docs) => {
-
               if (err) {
                   return reply(Boom.wrap(err, 'Internal MongoDB error'));
               }
-
               reply(docs);
           });
-
       }
   });
 
@@ -34,18 +30,29 @@ exports.register = function(server, options, next) {
         db.place.findOne({
             _id:db.ObjectId(request.params.id)
         }, (err, doc) => {
-
             if (err) {
                 return reply(Boom.wrap(err, 'Internal MongoDB error'));
             }
-
             if (!doc) {
                 return reply(Boom.notFound());
             }
-
             reply(doc);
         });
+    }
+  });
 
+  // GET place by _id
+  server.route({
+    method: 'GET',
+    path: '/near/{geo*}',
+    handler: function (request, reply) {
+        var geoPart = request.params.geo.split('/');
+        db.place.find({geolocation:{$near:[parseFloat(geoPart[0]),parseFloat(geoPart[1])]}},(err, docs) => {
+            if (err) {
+                return reply(Boom.wrap(err, 'Internal MongoDB error'));
+            }
+            reply(docs);
+        });
     }
   });
 
